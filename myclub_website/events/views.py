3ba71ca4534fb44e6282bf3d3b43185618e3b037
Julio Gazoli <julio_gazoli@hotmail.com>
2023-03-08 20:51:36 -0300

    Adicionado templates e configurado Bootstrap
    How To Use Templates and Custom HTML - Django Wednesdays #3

--- myclub_website/events/views.py
+++ myclub_website/events/views.py
@@ -3,7 +3,7 @@ import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
 
-def home(request, year, month):
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
 	name = "John"
 	month = month.capitalize()
 
@@ -21,7 +21,7 @@ def home(request, year, month):
 	# Get current time
 	time = now.strftime('%I:%M %p')
 
-	return render(request, 'home.html', 
+	return render(request, 'events/home.html', 
 		{
 			"name": name,
 			"year": year,
