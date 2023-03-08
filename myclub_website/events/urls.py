5f955ac6d909202dc7360781d68c32cf4ed48bd7
Julio Gazoli <julio_gazoli@hotmail.com>
2023-03-08 18:58:06 -0300

    Adicionado calendario HTML
    How To Add A Calendar To Your Website - Django Wednesdays #2

--- myclub_website/events/urls.py
+++ myclub_website/events/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.home, name="home"),
+	# int: numbers
+	# str: strings
+	# path: whole urls /
+	# slug: hyphen-and_underscores_stuff
+	# UUID: universally unique identifier
+
+	# path('', views.home, name="home"),
+	path('<int:year>/<str:month>/', views.home, name="home"),
 ]
