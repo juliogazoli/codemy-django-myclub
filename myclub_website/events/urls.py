774effe69b04995c5fe2f9434ef102b11804a95e
Julio Gazoli <julio_gazoli@hotmail.com>
2023-03-10 21:05:59 -0300

    Capturando dados do banco e apresentando na pagina
    Fetch Data From a Database And Output To A Webpage - Django Wednesdays #5

--- myclub_website/events/urls.py
+++ myclub_website/events/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	# Path Converters
 	# int: numbers
 	# str: strings
 	# path: whole urls /
@@ -10,4 +11,5 @@ urlpatterns = [
 
 	path('', views.home, name="home"),
 	path('<int:year>/<str:month>/', views.home, name="home"),
+	path('events', views.all_events, name="list-events"),
 ]
