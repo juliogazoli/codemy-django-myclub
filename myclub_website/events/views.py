a13b1c6f1d519190e800476e33f8f93ac8ac70cb
Julio Gazoli <julio_gazoli@hotmail.com>
2023-03-15 18:14:37 -0300

    Adicionado Form para cadastro de Venues
    How To Add Database Forms To A Web Page - Django Wednesdays #7

--- myclub_website/events/views.py
+++ myclub_website/events/views.py
@@ -2,12 +2,29 @@ from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
+from django.http import HttpResponseRedirect
 from .models import Event
+from .forms import VenueForm
+
+def add_venue(request):
+	submitted = False
+	if request.method == "POST":
+		form = VenueForm(request.POST)
+		if form.is_valid():
+			form.save()
+		return HttpResponseRedirect('/add_venue?submitted=True')
+	else:
+		form = VenueForm
+		if 'submitted' in request.GET:
+			submitted = True
+
+
+	return render(request, 'events/add_venue.html', {'form': form, 'submitted': submitted,})
 
 def all_events(request):
 	event_list = Event.objects.all()
 	return render(request, 'events/event_list.html', 
-		{'event_list': event_list})
+		{'event_list': event_list,})
 
 def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
 	name = "John"
@@ -35,6 +52,6 @@ def home(request, year=datetime.now().year, month=datetime.now().strftime('%B'))
 			"month_number": month_number,
 			"cal": cal,
 			"current_year": current_year,
-			"time": time
+			"time": time,
 		}
 	)
